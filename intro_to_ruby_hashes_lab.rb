0a2d839a547f4382a09213658d8f949eceaf29ef
Michael McDonald <mchlmcdnld7@gmail.com>
2020-02-16 21:01:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
   {railroads: {
     pieces: 4,
     rent_in_dollars: {
-      one_pieced_owned: 25, 
+      one_pieced_owned: [], 
       key2: [], 
       key3: [], 
       key4: []
