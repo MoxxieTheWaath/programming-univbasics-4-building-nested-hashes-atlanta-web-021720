6ae6c332e4ee9ea6ba2f086f941fc289f4fa0d0b
Michael McDonald <mchlmcdnld7@gmail.com>
2020-02-16 20:57:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
       one_pieced_owned: 25, 
       key2: [], 
       key3: [], 
-      key4: []}
+      key4: []},
     names: {
       name1: [], 
       name2: [], 
